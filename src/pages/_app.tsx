5cea9c6fbc2265e26d080798623caba869dd8b49
Vitaliy <vitaliydps4@gmail.com>
2023-03-15 19:27:37 +0100

    create aliases

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/assets/styles/globals.css'
+import type {AppProps} from 'next/app'
+import '../firebase/clientApp'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+	return <Component {...pageProps} />
 }

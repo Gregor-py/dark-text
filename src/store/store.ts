6b120e7c03a7c77b6a97325ce4012c5bc07008c0
Vitaliy <vitaliydps4@gmail.com>
2023-03-16 18:35:11 +0100

    Create redux slice for auth modal

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { authModalReducer } from '@/store/auth-modal/AuthModalSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
-  reducer: {},
-})
+  reducer: combineReducers({
+    authModal: authModalReducer
+  }),
+  devTools: process.env.NEXT_PUBLIC_ENV === 'dev'
+});
 export type RootState = ReturnType<typeof store.getState>
-
 export type AppDispatch = typeof store.dispatch
